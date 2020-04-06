cad5217ddfd46b8f470b1404f402d9ff2b7e928c
pbezpal <bezpalko@infotek.ru>
2020-04-06 22:32:33 +0300

    Перенёс данные для тестирования

--- src/main/java/data/CommentsData.java
+++ src/main/java/data/CommentsData.java
@@ -8,11 +8,11 @@ public interface CommentsData {
     String config = "Comments.properties";
 
     /***** Параметры для создания канала на клиенте ******/
-    String CLIENT_TYPE_COMMENTS_CHANNELS = getProperty("CLIENT_TYPE_COMMENTS_CHANNELS", config);
-    String CLIENT_ITEM_NEW_CHANNEL = getProperty("CLIENT_ITEM_NEW_CHANNEL", config);
-    String CLIENT_NAME_CHANNEL_PUBLIC = getProperty("CLIENT_NAME_CHANNEL_PUBLIC", config);
-    String CLIENT_TYPE_CHANNEL_PUBLIC = getProperty("CLIENT_TYPE_CHANNEL_PUBLIC", config);
-    String CLIENT_NAME_CHANNEL_CLOSED = getProperty("CLIENT_NAME_CHANNEL_CLOSED", config);
-    String CLIENT_TYPE_CHANNEL_CLOSED = getProperty("CLIENT_TYPE_CHANNEL_CLOSED", config);
+    String CLIENT_TYPE_COMMENTS_CHANNELS = "Каналы";
+    String CLIENT_ITEM_NEW_CHANNEL = "Новый канал";
+    String CLIENT_NAME_CHANNEL_PUBLIC = "Public test channel";
+    String CLIENT_TYPE_CHANNEL_PUBLIC = "Публичный";
+    String CLIENT_NAME_CHANNEL_CLOSED = "Closed test channel";
+    String CLIENT_TYPE_CHANNEL_CLOSED = "Закрытый";
 
 }

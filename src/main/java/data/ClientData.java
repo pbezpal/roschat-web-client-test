34467104d73b09106ca77da6e27d7fe091039126
pbezpal <bezpalko@infotek.ru>
2020-04-28 18:56:17 +0300

    Попытка №14 последовательный запуск тестов

---
+++ src/main/java/data/ClientData.java
@@ -0,0 +1,9 @@
+package data;
+
+public interface ClientData {
+    String HOST_HUB = "10.10.199.45";
+    String HOST_SERVER = "testing2.ros.chat";
+    String PORT_SERVER = "1110";
+    String LOGIN_ADMIN_MS = "admin";
+    String PASSWORD_ADMIN_MS = "123456";
+}
